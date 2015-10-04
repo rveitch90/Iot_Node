6d516c8a189bbaecb5cbc2da204edda7790aeebd
azizbahri <azizbahri90@gmail.com>
2015-10-04 09:24:54 +0100

    remove debug files

--- Project/IoT_RTOS_base THUMB Debug/STM32_Startup.d
+++
@@ -1,3 +0,0 @@
-IoT_RTOS_base\ THUMB\ Debug/STM32_Startup.o: \
- C:/Users/aziz/AppData/Local/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v3/packages/targets/STM32/STM32_Startup.s \
- C:/Users/aziz/AppData/Local/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v3/packages/targets/STM32/STM32F429xx.vec
